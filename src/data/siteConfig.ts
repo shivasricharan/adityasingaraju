0908256284b745e964a08522bd158fd91bfbebb7
shivasricharan <shivacharan.s@gmail.com>
2026-08-19 14:32:46 +0530

    Remove postponed election dates

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -1,8 +1,5 @@
 export const siteConfig = {
   electionMode: true,
-  electionDates: '19–21 August 2026',
-  electionStart: '2026-08-19T00:00:00+05:30',
-  electionEnd: '2026-08-21T23:59:59+05:30',
   email: '',
   phone: '',
   social: {},
